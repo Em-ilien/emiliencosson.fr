ae3568bcf29d6b08025493f0b06371ac130422d9
Emilien Cosson <emilien@em-ilien.fr>
2024-08-17 12:29:38 +0200

    feat(homepage) add homepage infos and spinner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,192 @@
+<script>
+	import Spider from './Spider.svelte';
+</script>
+
 <svelte:head>
 	<title>Em-ilien.fr - Accueil</title>
 	<meta name="description" content="Site web personnel d'Emilien Cosson" />
 </svelte:head>
 
-<p>Accueil</p>
+<div class="home-ctn">
+	<div class="infos">
+		<div class="names">
+			<h1><b>Em_i</b><span>lien</span></h1>
+			<h2>Emilien Cosson</h2>
+		</div>
+		<ul>
+			<li>
+				<a href="https://geocartie.fr" target="_blank">
+					<span class="service-title">Géocartie</span>
+					<span class="service-description">Apprendre et découvrir les départements français</span>
+				</a>
+			</li>
+			<li>
+				<a href="blog">
+					<span class="service-title">Blog</span>
+					<span class="service-description">Lire les articles</span>
+				</a>
+			</li>
+			<li>
+				<a href="psw-g">
+					<span class="service-title">PasswordGenerator</span>
+					<span class="service-description">Générer un mot de passe</span>
+				</a>
+			</li>
+			<li>
+				<a href="clock">
+					<span class="service-title">Clock</span>
+					<span class="service-description">Lire l'heure</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+
+	<Spider />
+</div>
+
+<style>
+	.home-ctn {
+		height: 100vh;
+		background-color: #f7f9fc;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.infos {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		margin: 6em;
+		text-align: center;
+	}
+	.infos .names {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 0;
+		margin-top: 3em;
+	}
+	.infos .names > * {
+		margin: 0;
+	}
+	.infos h1 {
+		font-size: 4rem;
+		color: #333;
+	}
+	.infos h1 b {
+		font-weight: 600;
+	}
+	.infos h1 span {
+		font-style: italic;
+		color: #fff;
+		letter-spacing: 16px;
+		text-shadow:
+			-3px 0 #eee,
+			0 3px #eee,
+			3px 0 #eee,
+			0 -3px #eee;
+		font-size: 4rem;
+		margin-left: 16px;
+	}
+	.infos h2 {
+		color: #bbb;
+		font-size: 2em;
+		font-weight: 400;
+		line-height: 1.5em;
+	}
+
+	.infos ul {
+		list-style: none;
+		width: fit-content;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 0em 2.5em;
+	}
+	.infos ul li {
+		z-index: 30;
+	}
+	.infos ul li a {
+		text-decoration: none;
+		font-size: 1.25em;
+		display: block;
+		padding: 0.5em;
+		border-radius: 0.25em;
+		border: 1px solid;
+		margin: 1em 0;
+		width: fit-content;
+		border-color: rgba(0, 102, 204, 0.26);
+		max-width: calc(100% - 1em);
+		background: #ffffff50;
+
+		text-decoration: none;
+		color: #1e88e5;
+		font-size: 1.5rem;
+		display: inline-block;
+		padding: 15px 25px;
+		border-radius: 10px;
+		background-color: #ffffff;
+		box-shadow: 0 6px 15px rgba(0, 0, 0, 0.1);
+		transition:
+			background-color 0.3s ease,
+			box-shadow 0.3s ease,
+			transform 0.3s ease;
+		position: relative;
+		overflow: hidden;
+		width: 15em;
+		height: 6em;
+		display: flex;
+		flex-direction: column;
+		gap: 0.325em;
+	}
+	.infos ul li a:hover {
+		border-color: rgba(0, 102, 204, 1);
+		background-color: #e3f2fd;
+		box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+		transform: translateY(-5px);
+	}
+	.infos ul li a,
+	.infos ul li a:visited {
+		color: rgba(0, 102, 204);
+	}
+
+	.infos ul li a::before {
+		content: '';
+		position: absolute;
+		top: -50px;
+		left: -50px;
+		width: 150px;
+		height: 150px;
+		background-color: rgba(30, 136, 229, 0.1);
+		border-radius: 50%;
+		z-index: 1;
+		transition: transform 0.3s ease;
+	}
+
+	.infos ul li a:hover::before {
+		transform: scale(1.5);
+	}
+
+	.infos ul li a span {
+		position: relative;
+		z-index: 2;
+		display: block;
+	}
+
+	.infos ul li a span:first-child {
+		font-weight: 600;
+		font-size: 1.25em;
+	}
+
+	.infos ul li a span:last-child {
+		text-align: left;
+		color: #666;
+		font-size: 0.9em;
+	}
+
+	.infos ul li a:hover span {
+		color: #1565c0;
+	}
+</style>
---
+++ src/routes/Spider.svelte
@@ -0,0 +1,348 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		/*--------------------
+    Vars
+    --------------------*/
+		const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
+		const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+		const win = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		};
+
+		const mouse = {
+			x: win.w * 0.5,
+			y: win.h * 0.5,
+			lerpX: win.w * 0.5,
+			lerpY: win.h * 0.5,
+			stepX: 0,
+			stepY: 0,
+			oldStepX: 0,
+			oldStepY: 0,
+			angle: 0
+		};
+
+		const opts = {
+			cellSize: 40,
+			pawRadius: 94,
+			pawHeight: 200,
+			pawRandomStep: 0.2,
+			bodyHeight: 20,
+			spiderOffsetX: 30,
+			spiderOffsetY: 40,
+			shadowY: 0.3,
+			speed: 0.12,
+			stop: false,
+			jump: 0
+		};
+
+		let cols = 0;
+		let rows = 0;
+
+		/*--------------------
+    Lerp
+    --------------------*/
+		const lerp = (a: number, b: number, c: number) => {
+			return (1 - c) * a + c * b;
+		};
+
+		/*--------------------
+    Random
+    --------------------*/
+		const random = (start: number, range: number) => {
+			return start - range + Math.random() * range * 2;
+		};
+
+		/*--------------------
+    Resize
+    --------------------*/
+		const handleResize = () => {
+			win.w = window.innerWidth;
+			win.h = window.innerHeight;
+			canvas.width = win.w;
+			canvas.height = win.h;
+			cols = Math.round(win.w / opts.cellSize) + 1;
+			rows = Math.round(win.h / opts.cellSize) + 1;
+		};
+		handleResize();
+		window.addEventListener('resize', handleResize);
+
+		/*--------------------
+    Paws
+    --------------------*/
+		const Paws = [
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 },
+			{ x: win.w * 0.5, y: win.h * 0.5 }
+		];
+
+		/*--------------------
+    Clear
+    --------------------*/
+		const clear = () => {
+			ctx.clearRect(0, 0, win.w, win.h);
+		};
+
+		/*--------------------
+    Circle
+    --------------------*/
+		const circle = (x, y, r, c) => {
+			ctx.fillStyle = c;
+			ctx.beginPath();
+			ctx.ellipse(x, y, r, r, 0, 0, Math.PI * 2);
+			ctx.fill();
+			ctx.closePath();
+		};
+
+		/*--------------------
+    Rect
+    --------------------*/
+		const rect = (x: number, y: number, r: number, c: string) => {
+			ctx.fillStyle = c;
+			ctx.fillRect(x, y, r, r);
+		};
+
+		/*--------------------
+    Walk
+    --------------------*/
+		const walk = () => {
+			let ind = 0;
+			Array(10)
+				.fill()
+				.forEach((a, i) => {
+					const theta = i / 10;
+					if (i % 5 === 0) return;
+					const x = random(
+						mouse.x + cols * 0.5 + opts.pawRadius * Math.sin(theta * Math.PI * 2),
+						opts.pawRadius * opts.pawRandomStep
+					);
+
+					const y = random(
+						mouse.y + rows * 0.5 + opts.pawRadius * Math.cos(theta * Math.PI * 2),
+						opts.pawRadius * opts.pawRandomStep
+					);
+
+					gsap.to(Paws[ind++], {
+						x,
+						y,
+						duration: opts.speed,
+						ease: 'power3.out()',
+						delay: ((i + 2) % 4) * 0.1
+					});
+				});
+		};
+
+		/*--------------------
+    Check Step
+    --------------------*/
+		const checkStep = () => {
+			mouse.stepX = Math.round((mouse.x / win.w) * (cols - 1)) * opts.cellSize;
+			mouse.stepY = Math.round((mouse.y / win.h) * (rows - 1)) * opts.cellSize;
+
+			if (mouse.oldStepX !== mouse.stepX || mouse.oldStepY !== mouse.stepY) {
+				walk();
+			}
+
+			mouse.oldStepX = mouse.stepX;
+			mouse.oldStepY = mouse.stepY;
+			// rect(mouse.stepX, mouse.stepY, opts.cellSize, 'rgba(255, 0, 0, .1)')
+		};
+
+		/*--------------------
+    Draw Spider
+    --------------------*/
+		const drawSpider = () => {
+			const x = mouse.lerpX - opts.spiderOffsetX;
+			const y = mouse.lerpY - opts.spiderOffsetY;
+
+			// Shadows
+			Paws.forEach((p, i) => {
+				ctx.strokeStyle = '#eee';
+				ctx.lineWidth = 1;
+				ctx.beginPath();
+				ctx.moveTo(x, y + opts.bodyHeight);
+				const x2 = lerp(x, p.x, 0.5);
+				const y2 = lerp(y + opts.jump, p.y, opts.shadowY);
+				ctx.quadraticCurveTo(x2, y2, p.x, p.y);
+				ctx.stroke();
+				ctx.closePath();
+			});
+
+			// Paws
+			Paws.forEach((p, i) => {
+				ctx.strokeStyle = 'black';
+				ctx.lineWidth = 1;
+
+				ctx.beginPath();
+				ctx.moveTo(x, y + opts.jump);
+				const x1 = lerp(x, p.x, 0.5);
+				const y1 = lerp(y + opts.jump, p.y - opts.pawHeight, 0.5);
+				ctx.quadraticCurveTo(x1, y1, p.x, p.y);
+				ctx.stroke();
+				ctx.closePath();
+			});
+
+			// Head
+		};
+
+		/*--------------------
+    Draw Mouse
+    --------------------*/
+		const drawMouse = () => {
+			mouse.lerpX = lerp(mouse.lerpX, mouse.x, 0.1);
+			mouse.lerpY = lerp(mouse.lerpY, mouse.y, 0.1);
+
+			if (opts.stop) {
+				mouse.lerpX = win.w * 0.5;
+				mouse.lerpY = win.h * 0.5;
+				mouse.x = win.w * 0.5;
+				mouse.y = win.h * 0.5;
+			}
+			checkStep();
+		};
+
+		/*--------------------
+    Draw
+    --------------------*/
+		const draw = (t) => {
+			clear();
+			drawMouse();
+			drawSpider();
+			requestAnimationFrame(draw);
+		};
+		draw();
+
+		/*--------------------
+    GUI
+    --------------------*/
+		const gui = new dat.GUI();
+		const f = gui.addFolder('Settings');
+		f.add(opts, 'spiderOffsetX', -100, 100, 1);
+		f.add(opts, 'spiderOffsetY', -100, 100, 1);
+		f.add(opts, 'pawRadius', 0, 100, 1).onChange(walk);
+		f.add(opts, 'pawHeight', 0, 200, 1);
+		f.add(opts, 'pawRandomStep', 0, 1, 0.1);
+		f.add(opts, 'bodyHeight', 0, 100, 1);
+		f.add(opts, 'shadowY', 0, 1, 0.1);
+		f.add(opts, 'speed', 0, 0.3, 0.01);
+		f.add(opts, 'cellSize', 10, 200, 1).onChange(handleResize);
+		f.add(opts, 'stop');
+		f.close();
+
+		/*--------------------
+    Mouse
+    --------------------*/
+		const handleMouseMove = (e) => {
+			if (opts.stop) {
+				mouse.lerpX = win.w * 0.5;
+				mouse.lerpY = win.h * 0.5;
+				mouse.x = win.w * 0.5;
+				mouse.y = win.h * 0.5;
+			} else {
+				mouse.x = e.clientX;
+				mouse.y = e.clientY;
+			}
+		};
+		const handleTouchMove = (e) => {
+			mouse.x = e.touches[0].clientX;
+			mouse.y = e.touches[0].clientY;
+		};
+		const handleMouseDown = () => {
+			if (opts.stop) return;
+			gsap.killTweensOf(opts);
+			gsap.to(opts, {
+				jump: opts.bodyHeight,
+				duration: 0.5,
+				ease: 'power3.out'
+			});
+		};
+		const handleMouseUp = () => {
+			if (opts.stop) return;
+			gsap.killTweensOf(opts);
+			gsap.to(opts, {
+				jump: 0,
+				duration: 2,
+				ease: 'elastic(1.4, 0.1)'
+			});
+		};
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('touchstart', handleTouchMove);
+		window.addEventListener('touchmove', handleTouchMove);
+		window.addEventListener('mousedown', handleMouseDown);
+		window.addEventListener('mouseup', handleMouseUp);
+
+		/*--------------------
+    Credits
+    --------------------*/
+		const credits = () => {
+			const div = document.createElement('div');
+			div.classList.add('credits');
+			div.innerHTML = `
+          <a href="https://linktr.ee/supahfunk" target="_blank" rel="noreferrer">
+            <h1>Fabio Ottaviani</h1>
+            <h2>Supahfunk</h2>
+          </a>
+      `;
+			canvas.parentElement.appendChild(div);
+		};
+
+		credits();
+	});
+</script>
+
+<!--
+	  Instagram: https://www.instagram.com/supahfunk/
+	  Twitter: https://twitter.com/supahfunk
+	  -->
+<canvas id="canvas">
+	<script src="https://unpkg.co/gsap@3/dist/gsap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.9/dat.gui.min.js"></script>
+</canvas>
+
+<style>
+	canvas {
+		width: 100%;
+		overflow: hidden;
+		margin: 0;
+		height: 99vh;
+		padding: 0;
+		outline: none;
+		position: absolute;
+		z-index: 20;
+	}
+
+	:global(.credits) {
+		position: fixed;
+		z-index: 30;
+		bottom: 20px;
+		right: 20px;
+	}
+	:global(.credits a) {
+		text-decoration: none;
+		color: #000;
+	}
+	:global(.credits span) {
+		font-size: 11px;
+	}
+	:global(.credits h1) {
+		margin: -2px 0 0;
+		padding: 0;
+		font-size: 10px;
+		font-weight: normal;
+	}
+	:global(.credits h2) {
+		margin: -1px 0;
+		font-size: 14.1px;
+		font-weight: normal;
+	}
+	:global(.dg.ac) {
+		z-index: 50;
+	}
+</style>
